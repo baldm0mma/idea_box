71b0c807db1115330193d636f38563481b25a9ea
Katie Williams <kawilliams8@gmail.com>
2019-04-02 16:44:31 -0600

    Add initial JS queries and event listeners

---
+++ main.js
@@ -0,0 +1,17 @@
+// 	Query Selectors
+var form1Button = document.querySelector(".sidebar__form1--button");
+var form2Input = document.querySelector(".sidebar__form2--input");
+var form2Button = document.querySelector(".sidebar__form2--button");
+var creatorTitleInput = document.querySelector(".creator__form--title-input");
+var creatorBodyInput = document.querySelector(".creator__form--body-input");
+var creatorSaveButton = document.querySelector(".creator__form--button");
+var searchBox = document.querySelector(".creator__search");
+
+// Event Listeners
+form1Button.addEventListener("click", functionName);
+form2Input.addEventListener("keyup", functionName);
+form2Button.addEventListener("click", functionName);
+creatorTitleInput.addEventListener("keyup", functionName);
+creatorBodyInput.addEventListener("keyup", functionName);
+creatorSaveButton.addEventListener("click", functionName);
+searchBox.addEventListener("keyup", functionName);
