e0cac992fbf1a861bfad583af490f6181750eb31
Katie Williams <kawilliams8@gmail.com>
2019-04-02 17:16:11 -0600

    Add save button disabling function

--- main.js
+++ main.js
@@ -8,10 +8,25 @@ var creatorSaveButton = document.querySelector(".creator__form--button");
 var searchBox = document.querySelector(".creator__search");
 
 // Event Listeners
-form1Button.addEventListener("click", functionName);
-form2Input.addEventListener("keyup", functionName);
-form2Button.addEventListener("click", functionName);
-creatorTitleInput.addEventListener("keyup", functionName);
-creatorBodyInput.addEventListener("keyup", functionName);
-creatorSaveButton.addEventListener("click", functionName);
-searchBox.addEventListener("keyup", functionName);
+// form1Button.addEventListener("click", functionName);
+// form2Input.addEventListener("keyup", functionName);
+// form2Button.addEventListener("click", functionName);
+// creatorTitleInput.addEventListener("keyup", functionName);
+// creatorBodyInput.addEventListener("keyup", functionName);
+creatorTitleInput.addEventListener("keyup", saveButtonEnable);
+window.addEventListener("load", onLoad);
+// searchBox.addEventListener("keyup", functionName);
+console.log('test');
+function onLoad() {
+	console.log('test2');
+	creatorSaveButton.disabled = true;
+}
+
+function saveButtonEnable() {
+	console.log("test3");
+	if (creatorTitleInput.value != "") {
+		creatorSaveButton.disabled = false;
+	} else {
+		creatorSaveButton.disabled = true;
+	}
+}
