50ae2996ea67534aab92b025631aa2121b20b139
Ann Cerveny <cervann11@gmail.com>
2019-04-02 18:24:18 -0600

    Connect save button to JS

--- main.js
+++ main.js
@@ -8,25 +8,26 @@ var creatorSaveButton = document.querySelector(".creator__form--button");
 var searchBox = document.querySelector(".creator__search");
 
 // Event Listeners
+window.addEventListener("load", onLoad);
 // form1Button.addEventListener("click", functionName);
 // form2Input.addEventListener("keyup", functionName);
 // form2Button.addEventListener("click", functionName);
 // creatorTitleInput.addEventListener("keyup", functionName);
 // creatorBodyInput.addEventListener("keyup", functionName);
+
 creatorTitleInput.addEventListener("keyup", saveButtonEnable);
-window.addEventListener("load", onLoad);
+creatorBodyInput.addEventListener("keyup", saveButtonEnable);
+
 // searchBox.addEventListener("keyup", functionName);
-console.log('test');
+
 function onLoad() {
-	console.log('test2');
-	creatorSaveButton.disabled = true;
+creatorSaveButton.disabled = true;
 }
 
 function saveButtonEnable() {
-	console.log("test3");
-	if (creatorTitleInput.value != "") {
-		creatorSaveButton.disabled = false;
-	} else {
+	if (creatorTitleInput.value === "" || creatorBodyInput.value === "") {
 		creatorSaveButton.disabled = true;
+	} else {
+		creatorSaveButton.disabled = false;
 	}
 }
